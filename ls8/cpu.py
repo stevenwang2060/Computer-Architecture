5e3b9ddd3611fba094843f6dd400f161c1d3c31e
Steven Wang <stevenwang2060@gmail.com>
2020-08-18 21:33:58 -0700

    having import issues

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,22 +10,44 @@ class CPU:
         self.ram = [0] * 256 # The memory storage for RAM.
         self.reg = [0] * 8   # 8 new registers. 
         self.pc = 0          # The program counter.
+        self.op_size = 1
+        self.running = True
+
+        # Instruction Handlers.
+        self.HLT = 0b00000001
+        self.LDI = 0b10000010
+        self.PRN = 0b01000111
+        self.ADD = 0b10100000
+        self.MUL = 0b10100010
 
     # Memory Address Register: holds the memory address we're reading or writing.
-    def ram_read(self, MAR): 
+    def ram_read(self, MAR):
         return self.ram[MAR]
     
     # Memory Data Register: holds the value to write or the value just read.
     def ram_write(self, MAR, MDR):
-        self.ram[MAR] = MDR
+        self.ram[MAR] =  MDR
 
-    def load(self):
+    def load(self, file_name):
         """Load a program into memory."""
 
-        address = 0
+        try: 
+            address = 0
+            with open(filename) as f:
+                for line in f:
+                    split_comment = line.split("#")
+                    # strip the whitespace and other chars
+                    n = split_comment[0].strip()
+                    if n == '':
+                        continue
+                    value = int(n, 2)
+                    self.ram[address] = value
+                    address += 1
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {filename} not found")
 
         # For now, we've just hardcoded a program:
-
+        """
         program = [
             # From print8.ls8
             0b10000010, # LDI R0,8
@@ -39,14 +61,16 @@ class CPU:
         for instruction in program:
             self.ram[address] = instruction
             address += 1
-
-
+        """
+        
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == MUL:
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -72,17 +96,26 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        running = True
-        while running:
-            IR = self.ram_read(self.pc)  # Instruction Register
-            if IR == 0b10000010:  # LDI R0,8
-                reg_num = self.ram_read(self.pc + 1)
-                value = self.ram_read(self.pc + 2)
-                self.reg[reg_num] = value
+        self.trace()
+
+        while self.running:
+            IR = self.ram_read(self.pc) # Instruction Register.
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if IR == self.ADD:
+                self.alu("ADD", operand_a, operand_b)
                 self.pc += 3
-            elif IR == 0b01000111:  # PRN R0
-                reg_num = self.ram_read(self.pc + 1)
-                print(self.reg[reg_num])
+            if IR == self.MUL:
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
+            elif IR == self.HLT:
+                self.running = False
+                self.pc += 1
+            elif IR == self.LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif IR == self.PRN:
+                num = self.reg[int(str(operand_a))]
+                print(num)
                 self.pc += 2
-            elif IR == 0b00000001:  # HLT
-                running = False
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,13 @@
 import sys
 from cpu import *
 
-cpu = CPU()
+sys.path.append('/ls8/cpu.py')
 
-cpu.load()
+cpu = CPU()
+if len(sys.argv) != 2:
+    print("No file parameter found...")
+    print("usage: python3 examples/print8.ls8")
+    sys.exit(1)
+load_file = sys.argv[1]
+cpu.load(load_file)
 cpu.run()
